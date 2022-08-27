c12f7de651b0e4a9c1dbc9a519db4b59c07f96db
Vinicius Bernardo <viniciuspibi@gmail.com>
2022-08-27 17:07:15 -0300

    Add unit tests templates

---
+++ tests/mul_test.c
@@ -0,0 +1,16 @@
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <cmocka.h>
+
+#include "../include/mul.h"
+
+/* A test case that does nothing and succeeds. */
+static void null_test_success(void **state) { (void)state; /* unused */ }
+int main(void) {
+  const struct CMUnitTest tests[] = {
+      cmocka_unit_test(null_test_success),
+  };
+  return cmocka_run_group_tests(tests, NULL, NULL);
+}
